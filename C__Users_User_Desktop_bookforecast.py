e161443537fc32bf3215990ed0c0aab7fc30e7e2
admason <60157744+admason@users.noreply.github.com>
2020-05-15 20:50:31 +0100

    draft optimiser percentage selector

---
+++ C__Users_User_Desktop_bookforecast.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+  
+    
+      
+        
+      
+
+      
+        
+            From To do
+
+        
+      
+
+        
+          
+            Restore
+            
+        
+    
+  
+
+  
+    
+      
+  
+  
+    
+  
+  
+    
+      Copy card link
+    
+      
+      
+      
+        Convert to issue
+      
+
+    
+
+    Edit note
+    
+      
+    Archive
+    
+
+    
+      
+      
+        Delete note
+      
+
+  
+
+
+  
+    
+  
+
+  
+    
+      
+        
+          #Optimise title?
+def optimise(sample_size,div,forecast):
+if sample_size>10:
+return 'Optimise'
+if div==1:
+    return 0.5*forecast
+elif div==2:
+    return 0.6*forecast
+elif div==3:
+    return 0.8*forecast
+else:
+    return 
+
+optimise(9,3,100)
+        
+      
+        
+          
+          #Optimise title?
+
+def optimise(sample_size,div,forecast):
+    if sample_size>10:
+        return 'Optimise'
+    
+    if div==1:
+        return 0.5*forecast
+    elif div==2:
+        return 0.6*forecast
+    elif div==3:
+        return 0.8*forecast
+    else:
+        return 
+
+optimise(9,3,100)
+    
+    Added by admason
+  
+
+
+
+
+
+
+        Press enter to move this card
+    
+
+  
+
+
+# In[2]:
+
+
+def optimise(sample_size,div,forecast):
+if sample_size>10:
+return 'Optimise'
+if div==1:
+    return 0.5*forecast
+elif div==2:
+    return 0.6*forecast
+elif div==3:
+    return 0.8*forecast
+else:
+    return 
+
+optimise(9,3,100)
+
+
+# In[3]:
+
+
+def optimise(sample_size,div,forecast):
+    if sample_size>10:
+        return 'Optimise'
+    
+    if div==1:
+        return 0.5*forecast
+    elif div==2:
+        return 0.6*forecast
+    elif div==3:
+        return 0.8*forecast
+    else:
+        return 
+
+optimise(9,3,100)
+
+
+# In[ ]:
+
+
+
+
